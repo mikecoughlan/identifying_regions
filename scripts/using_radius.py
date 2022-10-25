199a244089d05174b69596c9a395a95daf80f1d0
mikecoughlan <mike.k.coughlan@gmail.com>
2022-10-25 12:05:58 -0400

    debugging

--- scripts/using_radius.py
+++ scripts/using_radius.py
@@ -13,7 +13,8 @@ def getting_geo_coordinates(stations, station):
 
 	list_of_dfs = sorted(glob.glob('../../../../supermag/baseline/{0}/*.csv'.format(station), recursive=True))
 	df = pd.read_csv(list_of_dfs[0])
-	df = df['GEOLAT', 'GEOLON'][0]
+	print(df)
+	df = df[['GEOLAT', 'GEOLON']][:1]
 
 	df['station'] = station
 
