229b25c2946f6bc9b669e4f9212762802645c07b
mikecoughlan <mike.k.coughlan@gmail.com>
2022-10-25 10:26:55 -0400

    adding recursive statement for glob

--- scripts/using_radius.py
+++ scripts/using_radius.py
@@ -11,7 +11,7 @@ from shapely.geometry import Point, Polygon
 
 def getting_geo_coordinates(stations, station):
 
-	list_of_dfs = sorted(glob.glob('../../../../supermag/baseline/{0}/'.format(station)))
+	list_of_dfs = sorted(glob.glob('../../../../supermag/baseline/{0}/*.csv'.format(station), recursive=True))
 	print(list_of_dfs)
 	df = pd.read_csv(list_of_dfs[0])
 	df = df[['GEOLAT', 'GEOLON']][0]
