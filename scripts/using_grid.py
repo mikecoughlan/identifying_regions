e5743c855f73bbb4e6641af50a2a711c0dd4448a
Mike Coughlan <mike.k.coughlan@gmail.com>
2023-02-17 14:59:09 -0500

    changing grid parameters

--- scripts/using_grid.py
+++ scripts/using_grid.py
@@ -3,11 +3,12 @@ import math
 import os
 import pickle
 
-import contextily as cx
 import geopandas as gpd
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from shapely.geometry import Point, Polygon
 
 
@@ -38,8 +39,8 @@ def converting_from_degrees_to_km(lat_1, lon_1, lat_2, lon_2):
 
 def converting_grid_to_polygons(lat, lon, lat_deg, lon_deg):
 
-	lon_point_list = list(lat, lat, lat+lat_deg, lat+lat_deg)
-	lat_point_list = list(lon, lon+lon_deg, lon+lon_deg, lon)
+	lat_point_list = list([lat, lat, lat+lat_deg, lat+lat_deg])
+	lon_point_list = list([lon, lon+lon_deg, lon+lon_deg, lon])
 	polygon = Polygon(zip(lon_point_list, lat_point_list))
 	# polygon = gpd.GeoDataFrame(index=[0], crs='epsg:4326', geometry=geometry)
 
@@ -52,26 +53,30 @@ def finding_stations_in_grid(stations, lat_deg, lon_deg):
 
 	regions = {}
 	polys, num = [], []
+	i=0
 	for lat in lat_grid:
 		for lon in lon_grid:
 			stations_in_region = stations[(stations['GEOLAT']>=lat)&(stations['GEOLAT']<lat+lat_deg)
 											&(stations['GEOLON']>=lon)&(stations['GEOLON']<lon+lon_deg)]
 			stations_in_region = stations_in_region['station'].tolist()
 
-			poly = converting_grid_to_polygons(df)
-			regions['region_{0}'.format(i)] = {}
-			regions['region_{0}'.format(i)]['shape'] = poly
-			regions['region_{0}'.format(i)]['stations'] = stations_in_region
-			regions['region_{0}'.format(i)]['num_stations_in_region'] = len(stations_in_region)
-			polys.append(poly)
-			num.append(len(stations_in_region))
+			if len(stations_in_region) > 0:
 
-	with open('outputs/identified_regions_ver1.pkl', 'wb') as f:
+				poly = converting_grid_to_polygons(lat, lon, lat_deg, lon_deg)
+				regions['region_{0}'.format(i)] = {}
+				regions['region_{0}'.format(i)]['shape'] = poly
+				regions['region_{0}'.format(i)]['stations'] = stations_in_region
+				regions['region_{0}'.format(i)]['num_stations_in_region'] = len(stations_in_region)
+				polys.append(poly)
+				num.append(len(stations_in_region))
+				i+=1
+
+	with open('../outputs/identified_regions_ver1.pkl', 'wb') as f:
 		pickle.dump(regions, f)
 	print(regions)
 	gdf = pd.DataFrame({'geometry':polys,
 						'num_stations_in_region': num})
-	gdf = gpd.GeoDataFrame(gdf, geometry=gdf.geometry)
+	gdf = gpd.GeoDataFrame(gdf, geometry=gdf.geometry, crs='epsg:4326')
 	regions['plotting_gdf'] = gdf
 
 	return regions
@@ -80,19 +85,26 @@ def finding_stations_in_grid(stations, lat_deg, lon_deg):
 def plotting_regions(regions):
 
 	df = regions['plotting_gdf']
-	ax = plt.figure(figsize=(10,7))
+	fig = plt.figure(figsize=(10,7))
+	ax = plt.subplot(111)
+	ax.set_title('Num Stations in 5$^\circ$ x 5$^\circ$ Bins')
 	world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-	newdf = world.overlay(df, how='union')
+	world.set_crs('epsg:4326')
+	world.boundary.plot(ax=ax, zorder=0)
+
+	divider = make_axes_locatable(ax)
+	cax = divider.append_axes("right", size="5%", pad=0.1)
+
+	df.plot(column='num_stations_in_region', ax=ax, legend=True, cax=cax, zorder=1)
 	# world.plot(ax=ax)
 	# df.plot(ax=ax, column='num_stations_in_region', legend=True)
-	newdf.plot(column='num_stations_in_region', legend=True)
-	plt.savefig('plots/finding_regions_grid.png')
+	plt.savefig('../plots/finding_regions_grid.png')
 
 
 
 def main():
 
-	if not os.path.isfile('outputs/station_geo_locations.csv'):
+	if not os.path.isfile('../outputs/station_geo_locations.csv'):
 
 		all_stations = [name for name in os.listdir('../../../../supermag/baseline/')]
 		stations = pd.DataFrame()
@@ -102,10 +114,10 @@ def main():
 		stations.to_csv('outputs/station_geo_locations.csv', index=False)
 
 	else:
-		stations = pd.read_csv('outputs/station_geo_locations.csv')
+		stations = pd.read_csv('../outputs/station_geo_locations.csv')
 	stations['GEOLON'] = (stations['GEOLON'] + 180) % 360 - 180 # redefining the (0,360) geolon to a (-180,180) coordinate system
 
-	regions = finding_regions(stations)
+	regions = finding_stations_in_grid(stations, 5, 5)
 	plotting_regions(regions)
 
 
